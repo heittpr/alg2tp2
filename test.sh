6095095378c3650f7c790922ed14f17278f47449
Heitor <heitorleite.dev@gmail.com>
2025-07-05 21:03:34 -0300

    reorganize test data

--- test.sh
+++ test.sh
@@ -2,16 +2,15 @@
 
 TP2="./bin/tp2"
 TIMEOUT="30.1m"
-TESTS="tests"
+TESTS="data/tests"
 
-echo "solver,testcase,expected,answer,time,memory"
+echo "solver,testcase,answer,time,memory"
 
 for solver in "greedy" "fptas 0.2" "fptas 0.1" "fptas 0.05" "bnb"; do
   for input in "$TESTS/"*.in; do
     testcase=$(basename "$input" .in)
-    expected=$(cat "$TESTS/$testcase.out")
     output=$(timeout "$TIMEOUT" "$TP2" $solver < "$input") || output=",,"
 
-    echo "$solver,$testcase,$expected,$output"
+    echo "$solver,$testcase,$output"
   done
 done
