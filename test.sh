dc3f3d8b650e398d97badf6075a08bcbda41b4bf
Heitor <heitorleite.dev@gmail.com>
2025-07-05 22:01:37 -0300

    add test data

--- test.sh
+++ test.sh
@@ -6,7 +6,7 @@ TESTS="data/tests"
 
 echo "solver,testcase,answer,time,memory"
 
-for solver in "greedy" "fptas 0.2" "fptas 0.1" "fptas 0.05" "bnb"; do
+for solver in "greedy" "fptas 0.5" "fptas 0.25" "fptas 0.1" "bnb"; do
   for input in "$TESTS/"*; do
     testcase=$(basename "$input")
     output=$(timeout "$TIMEOUT" "$TP2" $solver < "$input") || output=",,"
