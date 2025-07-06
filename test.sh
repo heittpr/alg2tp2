0eaf2cc9dac4801244fb3f7237a93a40353bdf9b
Heitor <heitorleite.dev@gmail.com>
2025-07-05 21:07:21 -0300

    remove .in suffix from testcases

--- test.sh
+++ test.sh
@@ -7,10 +7,9 @@ TESTS="data/tests"
 echo "solver,testcase,answer,time,memory"
 
 for solver in "greedy" "fptas 0.2" "fptas 0.1" "fptas 0.05" "bnb"; do
-  for input in "$TESTS/"*.in; do
-    testcase=$(basename "$input" .in)
+  for input in "$TESTS/"*; do
+    testcase=$(basename "$input")
     output=$(timeout "$TIMEOUT" "$TP2" $solver < "$input") || output=",,"
-
     echo "$solver,$testcase,$output"
   done
 done
